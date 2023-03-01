3ef71abcb98ebc755de8b42fe63140e9b9cff3d3
Jamie Read <james.read@sideshowagency.com>
2023-03-01 17:56:15 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MyComponent from "./components/myComponent";
 
 function App() {
   return (
     <div className="App">
+      {/* <MyComponent /> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
